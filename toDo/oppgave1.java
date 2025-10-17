152ee566319a03e66f3f21274f50bb86e127eebd
simulert <gunnargustad@hotmail.com>
2025-10-17 14:04:27 +0200

    .

--- toDo/oppgave1.java
+++ toDo/oppgave1.java
@@ -1,4 +1,6 @@
-import
+import kilder.oppgave1.DailyPower;
+import kilder.oppgave1.DayMain;
+import kilder.oppgave1.DayPowerData;
 
 public class daglig_stromforbrok_og_pris {
     public static void printPowerPrices(double[] prices) {
